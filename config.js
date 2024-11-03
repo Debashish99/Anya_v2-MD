ee9c67b9886b25b85a61d1b517d64e44dc6e597a
Debashish99 <hukaibrinchi@gmail.com>
2024-11-03 13:16:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,27 +10,27 @@ module.exports = {
      */
     botname: process.env.BotName || "Qᴜᴇᴇɴ ᴀɴʏᴀ ᴠ2",
     footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐀𝐧𝐲𝐚 𝐁𝐨𝐭",
-    prefa: process.env.Prefix || "-",
+    prefa: process.env.Prefix || "!",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "𝓡𝓲𝓸(リオ)🍃🌸",
+    ownernumber: process.env.Owner_Number || "917636987434",
+    instagramId: process.env.Insta || "unknown_____11__d",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
+    author: process.env.Author || "𝓡𝓲𝓸(リオ)🍃🌸",
     packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://dpatar718:<db_deba1432>@rioyang25.6ehfa.mongodb.net/?retryWrites=true&w=majority&appName=Rioyang25",
+    sessionId: process.env.SESSION_ID || "DVl2et7M49IKX_A_N_Y_A_V_2_HBO52Wl",
 
     /**
      * thumbnails and medias
